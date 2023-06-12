2af777435aee9a7366c49e10f43c647d88e0404f
debussysuh <debussysuh@gmail.com>
2023-06-12 15:34:28 +0900

    first commit

---
+++ src/main/java/com/kr/tooit/TooitApplication.java
@@ -0,0 +1,13 @@
+package com.kr.tooit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TooitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/tooit/TooitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.tooit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
