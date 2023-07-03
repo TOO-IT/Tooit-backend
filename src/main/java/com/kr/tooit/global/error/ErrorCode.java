c4f886658274d97be1b2ff97ec3f35eb4d0cdf1e
techhan <it_hs@naver.com>
2023-07-03 14:06:44 +0900

    feat/error custom exception 추가

---
+++ src/main/java/com/kr/tooit/global/error/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.kr.tooit.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "ACCOUNT-001", "사용자를 찾을 수 없습니다."),
+    HAS_NICKNAME(HttpStatus.BAD_REQUEST, "ACCOUNT-002", "중복된 닉네임입니다."),
+    BAD_NICKNAME(HttpStatus.BAD_REQUEST, "ACCOUNT-003", "올바르지 않은 닉네임입니다. 닉네임은 1~15자 이하로 작성해주세요.")
+
+    ;
+
+
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
