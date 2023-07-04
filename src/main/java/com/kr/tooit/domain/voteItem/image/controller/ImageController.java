6fa526df1360ed3bf2a8a2b03529d79c30c70094
debussysuh <debussysuh@gmail.com>
2023-07-04 09:39:13 +0900

    Feat: VoteImage Upload Controller

---
+++ src/main/java/com/kr/tooit/domain/voteItem/image/controller/ImageController.java
@@ -0,0 +1,34 @@
+package com.kr.tooit.domain.voteItem.image.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kr.tooit.domain.voteItem.image.service.ImageService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ImageController {
+	private final ImageService imageService;
+
+	@GetMapping("/upload")
+	public String getUpload() {
+		return "upload";
+	}
+
+	@PostMapping("/sample")
+	public ResponseEntity<Object> uploadImagesSample(
+		@RequestPart(value = "files") List<MultipartFile> multipartFiles) {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(imageService.uploadImagesSample(multipartFiles));
+	}
+}
