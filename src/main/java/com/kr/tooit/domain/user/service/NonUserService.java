48cf944b180c9c018f365334f30ce069c598913b
techhan <it_hs@naver.com>
2023-08-26 20:15:57 +0900

    chore : nonUser createDate 어노테이션 추가 및 save 로직 수정

--- src/main/java/com/kr/tooit/domain/user/service/NonUserService.java
+++ src/main/java/com/kr/tooit/domain/user/service/NonUserService.java
@@ -5,6 +5,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class NonUserService {
@@ -13,7 +15,11 @@ public class NonUserService {
 
     @Transactional
     public void save(NonUsers nonUser) {
-        nonUserRepository.save(nonUser);
+        Optional<NonUsers> findNonUser = nonUserRepository.findById(nonUser.getIp());
+
+        if(findNonUser.isEmpty()) {
+            nonUserRepository.save(nonUser);
+        }
     }
 
 }
